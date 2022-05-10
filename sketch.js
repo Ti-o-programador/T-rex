91a58acde2c7ca7a4070033187138a7881b70b26
andressalessa <lessadasilveiraandressa@gmail.com>
2022-05-10 20:36:28 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
 var cacto1, cacto2, cacto3, cacto4, cacto5, cacto6;
-var cacto;
-
+var cacto, cactoGroup, cloudGroup;
 var score;
 
+const PLAY = 1;
+const END = 0;
+var gameState = PLAY;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
@@ -18,7 +20,6 @@ function preload(){
   cacto4 = loadImage("obstacle4.png");
   cacto5 = loadImage("obstacle5.png");
   cacto6 = loadImage("obstacle6.png");
-  
 }
 
 function setup() {
@@ -28,32 +29,33 @@ function setup() {
   //crie um sprite de trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
-  trex.scale = 0.45;
+  trex.scale = 0.5;
   
   //crie sprite ground (solo)
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
-  ground.velocityX = -4;
   
   //crie um solo invisível
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
-  
-  //gerar números aleatórios
-  var rand =  Math.round(random(1,100))
-  console.log(rand)
 
+  cactoGroup = new Group();
+  cloudGroup = new Group();
 }
 
 function draw() {
   //definir cor do plano de fundo
-  background(180);
+  background("black");
+
   
- 
-  
-  
-  
+  if (gameState === PLAY) {
+    ground.velocityX = -4;
+
+  } else if (gameState === END) {
+    ground.velocityX = 0;
+  }
+
   // pulando o trex ao pressionar a tecla de espaço
   if(keyDown("space")&& trex.y >= 100) {
     trex.velocityY = -10;
@@ -85,6 +87,7 @@ function spawnClouds(){
     cloud.y=Math.round(random(10,60));
     cloud.depth=trex.depth;
     trex.depth=trex.depth+1;
+    cloudGroup.add(cloud);
   }
   
 }
@@ -92,34 +95,33 @@ function spawnClouds(){
 function createCactos()
 {
   if(frameCount % 80 === 0){
-    var cacto = createSprite(600,165,8,20);
-    cacto.velocityX = -3;
-    
-    var ran = Math.round(random(1,6));
-    switch (ran) {
-      case 1 :
+    var rand = Math.round(random(1, 6));
+    var cacto = createSprite(600,165,10,40);
+
+    switch (rand) {
+      case 1:
         cacto.addImage(cacto1);
         break;
-      case 2 :
+      case 2:
         cacto.addImage(cacto2);
         break;
-      case 3 :
+      case 3:
         cacto.addImage(cacto3);
         break;
-      case 4 :
+      case 4:
         cacto.addImage(cacto4);
         break;
-      case 5 :
+      case 5:
         cacto.addImage(cacto5);
         break;
-      case 6 :
+      case 6:
         cacto.addImage(cacto6);
         break;
-    
-      default:
-        break;
     }
+
+    cacto.velocityX = -3;
     cacto.scale = 0.5;
+    cactoGroup.add(cacto);
   }
 }
 
